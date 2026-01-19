478aa04e5094d13a25c70b8fdbbf385c64a98626
Jorba56 <56415377+Jorba56@users.noreply.github.com>
2026-01-19 12:24:31 +0100

    terminar

--- raquel/cookies/script.js
+++ raquel/cookies/script.js
@@ -2,25 +2,28 @@ let carro=[];
 import productos from './productos.js';
 let lista=document.getElementById('productos2');
 let listaCarro=document.getElementById('carrito2');
-function pintarProductos(){
+let searchInput = document.getElementById('searchInput');
+searchInput.addEventListener("keyup", function() {  filterData(productos);
+});
+function pintarProductos(array){
 lista.innerHTML="";
-    for(let i=0; i<productos.length;i++) {
+    for(let i=0; i<array.length;i++) {
         lista.innerHTML+=`
         <div class="tarjeta">
         <div class="producto${i+1}">
-            <img src="${productos[i].imagen}">
-            <h3 id="name_prod">${productos[i].name_prod}</h3>
-            <p id="precio">${productos[i].precio}€</p>
+            <img src="${array[i].imagen}">
+            <h3 id="name_prod">${array[i].name_prod}</h3>
+            <p id="precio">${array[i].precio}€</p>
             <button id="addProd${i+1}">Añadir al carro</button>
             <br>
         </div>
         </div>
         `;
     };
-    
+
   }
 function leerCantidades(){
-  pintarProductos();
+  pintarProductos(productos);
     for (let i=0;i<productos.length;i++){
         const boton = document.getElementById(`addProd${i+1}`);
         boton.addEventListener('click', ()=>{
@@ -31,18 +34,16 @@ function leerCantidades(){
             productos[i].cantidad=1;
           }
             console.log(carro);
-            pintarCarro();
+            pintarCarro(carro);
         });
     };
-    
   }
-
-function pintarCarro(){
-
+function pintarCarro(carro){
 listaCarro.innerHTML="";
-if (carro.length>0){
+if (carro.length===0){
+    listaCarro.innerHTML+="<p>No hay productos en tu carro.</p>";
+  }else {
     for(let i=0; i<carro.length;i++) {
-      let precioTotal=carro[i].precio*carro[i].cantidad;
         listaCarro.innerHTML+=`
         <div class="tarjeta">
         <div class="carro${i+1}">
@@ -51,17 +52,14 @@ if (carro.length>0){
             <p id="cantidad">Cantidad: ${carro[i].cantidad}</p>
             <button id="sumar${i+1}">+</button>
             <button id="restar${i+1}">-</button>
-            
             <br>
         </div>
         </div>
         `;
     };
     listaCarro.innerHTML+=`<h3>Precio total del carrito: ${carro.reduce((total, item) => total + (item.precio * item.cantidad), 0).toFixed(2)}€</h3>`;
-  }else{
-    listaCarro.innerHTML="<p>No hay productos en tu carro.</p>";
   }
-  actualizarCarro(carro);
+    actualizarCarro(carro);
 }
 function actualizarCarro(carro){
   for (let i=0;i<carro.length;i++){
@@ -71,8 +69,7 @@ function actualizarCarro(carro){
             carro[i].cantidad+=1;
             console.log(carro);
           }
-            
-            pintarCarro();
+            pintarCarro(carro);
         });
         const menos = document.getElementById(`restar${i+1}`);
         menos.addEventListener('click', ()=>{
@@ -82,9 +79,24 @@ function actualizarCarro(carro){
           }if(carro[i].cantidad===0){
             carro.splice(i,1);
           }
-            pintarCarro();
-        });
-        
+            pintarCarro(carro);
+        });   
   }
 }
 document.addEventListener('DOMContentLoaded', leerCantidades);
+function filterData(productos) {
+  let text = searchInput.value.toLowerCase().trim() ;
+  var filtered = [];
+  for (let i = 0; i < productos.length; i++) {
+    let prod = productos[i];
+    // Search in multiple properties
+    if (prod.name_prod.toLowerCase().includes(text) ||
+    prod.precio.toString().includes(text)) 
+                filtered.push(prod);
+   }
+  // Render filtered results to the list element 
+  pintarProductos(filtered);
+  leerCantidades();
+}
+searchInput.addEventListener("keyup", function() {  filterData(productos);
+});
